e0a95fd3a361462ae7fce7135535f15e9e7fc183
Bipin Swarnkar <vipinswarnkar1989@gmail.com>
2016-06-14 16:14:32 +0530

    Added $scope.img for showing loading image and output image.

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,8 @@
 angular.module('fileUploadDemo',['ngFileUpload'])
 .controller('HomeCtrl',['Upload','$scope',function(Upload,$scope){
-	
+	$scope.img = false;
 		$scope.upload = function(user){
+			$scope.img = '/loadingAnimation.gif';
 		if(user.file){
 			Upload.upload({
 				url: 'http://localhost:3000/image-upload',
@@ -9,6 +10,7 @@ angular.module('fileUploadDemo',['ngFileUpload'])
 			}).then(function(res){
 				if(res.data.success){
 					alert(res.data.msg);
+					$scope.img = 'uploads/'+res.data.img_path;
 				}
 				else{
 					alert(res.data.msg);
